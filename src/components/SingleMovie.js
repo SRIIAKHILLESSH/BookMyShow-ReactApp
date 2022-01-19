e6247c6b52240fa41c3cc98a26f573597b14ab3d
SRIIAKHILLESSH <52933281+SRIIAKHILLESSH@users.noreply.github.com>
2022-01-19 23:56:16 +0530

    aa

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -18,6 +18,9 @@ function SingleMovie() {
                                 <Col id={mov.id} key={mov.id} xs={6} md={4} lg={3} style={{marginBottom:"2%"}}>
                                     <Card>
                                         <Card.Img variant="top" src={mov.image} />
+                                        <img src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FVenom_(2018_film)&psig=AOvVaw3lcaMI_5q5HLtoIlHDDG2m&ust=1642701741576000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNCE9ICzvvUCFQAAAAAdAAAAABAD" alt = "venom">
+                                     </img> 
+                                 
                                         <Card.Body>
                                         <Card.Title>{mov.title}</Card.Title>
                                         <Card.Text>{mov.actor}</Card.Text>
--- src/components/SingleMovieFetch.js
+++ src/components/SingleMovieFetch.js
@@ -30,6 +30,9 @@ function SingleMovieFetch() {
                             <Col id={mov._id} key={mov._id} xs={6} md={4} lg={3} style={{marginBottom:"2%"}}>
                                 <Card>
                                     <Card.Img variant="top" src={mov.imageurl} />
+                                    <img src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FVenom_(2018_film)&psig=AOvVaw3lcaMI_5q5HLtoIlHDDG2m&ust=1642701741576000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNCE9ICzvvUCFQAAAAAdAAAAABAD" alt = "venom">
+                                     </img> 
+                                 
                                     <Card.Body>
                                     <Card.Title>{mov.title}</Card.Title>
                                     <Card.Text>{mov.actor}</Card.Text>
