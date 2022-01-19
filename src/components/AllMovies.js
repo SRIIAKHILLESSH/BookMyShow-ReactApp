2bf24b4f5fdcb91a43db93f08a1adc4546821951
SRIIAKHILLESSH <52933281+SRIIAKHILLESSH@users.noreply.github.com>
2022-01-19 23:37:39 +0530

    ff

--- src/components/AllMovies.js
+++ src/components/AllMovies.js
@@ -13,6 +13,9 @@ function AllMovies() {
                         <Col id={mov.id} key={mov.id} xs={6} md={4} lg={3} style={{marginBottom:"2%"}}>
                             <Card style={{cursor:"pointer"}} onClick={()=> window.location.href="/movies/"+mov.id }>
                                 <Card.Img variant="top" src={mov.image} />
+                                 <img src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FVenom_(2018_film)&psig=AOvVaw3lcaMI_5q5HLtoIlHDDG2m&ust=1642701741576000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNCE9ICzvvUCFQAAAAAdAAAAABAD" alt = "venom">
+                                     </img> 
+                                 
                                 <Card.Body>
                                 <Card.Title>{mov.title}</Card.Title>
                                 <Card.Text>{mov.actor}</Card.Text>
