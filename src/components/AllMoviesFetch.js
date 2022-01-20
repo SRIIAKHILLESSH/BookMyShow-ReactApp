6e0823ddf7ce8db3db881f8b0ea4115f93da1569
SRIIAKHILLESSH <52933281+SRIIAKHILLESSH@users.noreply.github.com>
2022-01-20 14:31:43 +0530

    last commit

--- src/components/AllMoviesFetch.js
+++ src/components/AllMoviesFetch.js
@@ -20,21 +20,76 @@ function AllMoviesFetch() {
 
     return (
         <div>
-            <Container fluid style={{padding:"5%", background:"blue"}}>
+            <Container fluid style={{padding:"5%", background:"red"}}>
+            <div style={{background:"red", padding:"5%"}}>
+            <div style={{display:"flex", gap:"10px", justifyContent:"space-around"}}>
+            <div class = "wrapper text-white">
+                <div>
+                <Card.Img src="./rrr.png" alt="Card image" style={{height:"300px", width:"300px"}} />
+                
+                <div>
+                <div>
+                <div style={{background:"black"}}>
+                <Card.Title>RRR</Card.Title>
+                    Book a ticket now !
+                </div>
+                </div>
+                </div>
+            </div>
+
+            </div>
+            <div class = "wrapper text-white">
+                <div>
+                <Card.Img src="./akhanda.png" alt="Card image" style={{height:"300px", width:"300px"}} />
+                
+                <div>
+                <div>
+                <div style={{background:"black"}}>
+                <Card.Title>Akhanda</Card.Title>
+                    Book a ticket now !
+                </div>
+                </div>
+                </div>
+            </div>
+
+            </div>
+            
+            <Card className="bg-dark text-white" style={{height:"300px", width:"300px"}}>
+                        <Card.Img src="./liger.png" alt="Card image" style={{height:"300px", width:"300px"}}/>
+                        <div>
+                        <div style={{background:"black"}}>
+                            <Card.Title>Liger</Card.Title>
+                            Book a ticket now
+                            </div>
+                            </div>
+                    </Card>
+            </div>
+
+            
+
+            </div>
+
+            
+
+
+                    
                 <Row style={{textAlign:"center"}}>
                     {data.map((mov) =>
                         <Col id={mov._id} key={mov.id} xs={6} md={4} lg={3} style={{marginBottom:"2%"}}>
                             <Card style={{cursor:"pointer"}} onClick={()=> window.location.href="/movies/"+mov._id }>
                                 <Card.Img variant="top" src={mov.imageurl} />
+                                
+            
                                 </Card>
                     <Card className="bg-dark text-white" style={{height:"300px", width:"300px"}}>
                         <Card.Img src="./akhanda.png" alt="Card image" style={{height:"300px", width:"300px"}}/>
                          
                                  
                                 <Card.Body>
-                                <Card.Img src="./akhanda.png" alt="Card image" style={{height:"300px", width:"300px"}}/>
-                         
-                                <Card.Title>{mov.title}</Card.Title>
+                               
+                
+    
+                                <Card.Title>{AllMoviesFetch.title}</Card.Title>
                                 <Card.Text>{mov.actor}</Card.Text>
                                 </Card.Body>
                             </Card>
